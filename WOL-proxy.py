ef2518e47acc3b86d7160cc3e5dc87b37a023ffe
Michelli <Michelli@users.noreply.github.com>
2024-03-19 13:15:30 +0100

    Add machine name to MQTT topic

--- WOL-proxy.py
+++ WOL-proxy.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import os
 import re
+
 import paho.mqtt.client as mqtt
 from wakeonlan import send_magic_packet
 
@@ -11,6 +12,7 @@ MQTT_CLIENT_ID     = os.getenv('MQTT_CLIENT_ID',"WOL-proxy")
 MQTT_USERNAME      = os.getenv('MQTT_USERNAME',"")
 MQTT_PASSWORD      = os.getenv('MQTT_PASSWORD',"")
 MQTT_TOPIC_PREFIX  = os.getenv('MQTT_TOPIC_PREFIX',"WOL-proxy")
+MQTT_MACHINE_NAME  = os.getenv('MQTT_MACHINE_NAME',"raspberry")
 MQTT_QOS           = int(os.getenv('MQTT_QOS',1))
 WOL_BROADCAST_ADDR = os.getenv('WOL_BROADCAST_ADDR',"255.255.255.255")
 #print("All env vars read.")
@@ -19,18 +21,18 @@ WOL_BROADCAST_ADDR = os.getenv('WOL_BROADCAST_ADDR',"255.255.255.255")
 def on_connect(client, userdata, flags, rc):
     print(f"Connected to broker at {MQTT_BROKER_HOST}:{MQTT_BROKER_PORT} with result code {rc}: "+mqtt.connack_string(rc))
 
-    client.publish(MQTT_TOPIC_PREFIX+"/status",payload="Online",qos=1,retain=True)
+    client.publish(MQTT_TOPIC_PREFIX+"/"+MQTT_MACHINE_NAME+"/status",payload="Online",qos=1,retain=True)
 
     # subscribe to command topic
-    client.subscribe(MQTT_TOPIC_PREFIX+"/command", MQTT_QOS)
+    client.subscribe(MQTT_TOPIC_PREFIX+"/"+MQTT_MACHINE_NAME+"/command", MQTT_QOS)
 
 def on_subscribe(client, userdata, mid, granted_qos):
-    print(f"Subscribed to commands on topic \"{MQTT_TOPIC_PREFIX}/command\" with QOS {granted_qos[0]}.")
-    print(f"Wake-On-LAN proxy service started.")  
+    print(f"Subscribed to commands on topic \"{MQTT_TOPIC_PREFIX}/{MQTT_MACHINE_NAME}/command\" with QOS {granted_qos[0]}.")
+    print("Wake-On-LAN proxy service started.")  
 
 def on_disconnect(client, userdata, rc):
     if rc != 0:
-        print(f"Unexpected disconnection from broker (RC={rc}). Attempting to reconnect...")
+        print(f"Unexpected disconnection from broker (RC={rc}). Attempting to reconnect.")
 
 # callback for when the client receives a message on the subscribed topic
 def on_message(client, userdata, message):
@@ -40,14 +42,14 @@ def on_message(client, userdata, message):
         send_magic_packet(message.payload,ip_address=WOL_BROADCAST_ADDR)
         print(f"Magic packet sent to {message.payload}.")
     else:
-        print(f"Message payload has invalid mac address format!")
+        print(f"Message payload {message.payload} has invalid MAC address format")
 
 # set up mqtt client
 client = mqtt.Client(client_id=MQTT_CLIENT_ID)
 if MQTT_USERNAME and MQTT_PASSWORD:
     client.username_pw_set(MQTT_USERNAME,MQTT_PASSWORD)
     print("Username and password set.")
-client.will_set(MQTT_TOPIC_PREFIX+"/status", payload="Offline", qos=1, retain=True)    
+client.will_set(MQTT_TOPIC_PREFIX+"/"+MQTT_MACHINE_NAME+"/status", payload="Offline", qos=1, retain=True)    
 client.on_connect = on_connect # on connect callback
 client.on_message = on_message # on message callback
 client.on_disconnect = on_disconnect # on disconnect callback
